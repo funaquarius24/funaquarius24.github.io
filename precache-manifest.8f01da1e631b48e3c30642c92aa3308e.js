55aabb861e3f4082daa0d08b68aa1cbfc067804e
funaquarius24 <rjismail202@gmail.com>
2020-01-04 20:53:28 +0100

    test master commit

---
+++ precache-manifest.8f01da1e631b48e3c30642c92aa3308e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5041fd76804d825f3d1c716ce519f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7860b9acbaf28ab194dc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a978d95402da80698534",
+    "url": "/static/js/2.4d21f272.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/static/js/2.4d21f272.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7860b9acbaf28ab194dc",
+    "url": "/static/js/main.590a2791.chunk.js"
+  },
+  {
+    "revision": "057f97e2bb59c742f6f1",
+    "url": "/static/js/runtime-main.adff029d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
